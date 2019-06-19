30d3c969ca30631ca3fe93ec4c8010c9143f077a
Nick Harper <bignickharper@gmail.com>
2019-06-19 12:32:27 -0700

    scripting up the ojectives

--- Microscope/Assets/EffectControls.cs
+++ Microscope/Assets/EffectControls.cs
@@ -3,6 +3,7 @@
 public class EffectControls : MonoBehaviour
 {
     public GameObject camera;
+    public float[] zoomLevels = new float[3];
 
     void Start()
     {
@@ -10,6 +11,7 @@ public class EffectControls : MonoBehaviour
         blurScript.DownRes = -4;
         blurScript.Iterations = -6;
         gameObject.GetComponent<Renderer>().sharedMaterial.SetFloat("_Brightness", -1);
+        camera.transform.position = new Vector3(0,zoomLevels[0],0);
     }
 
     public void ChangeBrightness(float f)
@@ -29,6 +31,33 @@ public class EffectControls : MonoBehaviour
         blurScript.Iterations = (int)Mathf.Round(adj);
 
     }
+
+    public void TranslateX(float adj)
+    {
+
+    }
+
+    public void TranslateY(float adj)
+    {
+
+    }
+
+    public void Zoom(float level)
+    {
+        //check that focus is dialed in
+        //disable coarse focus on zoom level 2-3
+        SimpleBoxBlur blurScript = camera.GetComponent<SimpleBoxBlur>();
+        if (blurScript.DownRes == 0 && camera.transform.position.y == zoomLevels[0])
+        {
+            camera.transform.position = new Vector3(0,zoomLevels[1], 0 );
+        }
+
+        if (blurScript.DownRes == 0 && camera.transform.position.y == zoomLevels[1])
+        {
+            camera.transform.position = new Vector3(0,zoomLevels[2],0);
+        }
+    }
+
 
 
 }
