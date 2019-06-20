47332f3fbb13633379e5c7498d7a08c385e420a7
NickHarper <bignickharper@gmail.com>
2019-06-19 21:48:00 -0700

    put in the 3D asset

--- Microscope/Assets/EffectControls.cs
+++ Microscope/Assets/EffectControls.cs
@@ -3,7 +3,7 @@
 public class EffectControls : MonoBehaviour
 {
     public GameObject camera;
-    public float[] zoomLevels = new float[3];
+    public float[] zoomLevels = new float[3]{18f,9f,0.5f};
 
     void Start()
     {
@@ -11,7 +11,7 @@ public class EffectControls : MonoBehaviour
         blurScript.DownRes = -4;
         blurScript.Iterations = -6;
         gameObject.GetComponent<Renderer>().sharedMaterial.SetFloat("_Brightness", -1);
-        camera.transform.position = new Vector3(0,zoomLevels[0],0);
+        camera.transform.position = new Vector3(-10,18,-10);
     }
 
     public void ChangeBrightness(float f)
@@ -42,20 +42,21 @@ public class EffectControls : MonoBehaviour
 
     }
 
-    public void Zoom(float level)
+    public void Zoom(int level)
     {
+        camera.transform.position = new Vector3(-10,zoomLevels[level],-10);
         //check that focus is dialed in
         //disable coarse focus on zoom level 2-3
-        SimpleBoxBlur blurScript = camera.GetComponent<SimpleBoxBlur>();
-        if (blurScript.DownRes == 0 && camera.transform.position.y == zoomLevels[0])
-        {
-            camera.transform.position = new Vector3(0,zoomLevels[1], 0 );
-        }
+        //SimpleBoxBlur blurScript = camera.GetComponent<SimpleBoxBlur>();
+        //if (blurScript.DownRes == 0 && camera.transform.position.y == zoomLevels[0])
+        //{
+        //    camera.transform.position = new Vector3(0,zoomLevels[1], 0 );
+        //}
 
-        if (blurScript.DownRes == 0 && camera.transform.position.y == zoomLevels[1])
-        {
-            camera.transform.position = new Vector3(0,zoomLevels[2],0);
-        }
+        //if (blurScript.DownRes == 0 && camera.transform.position.y == zoomLevels[1])
+        //{
+        //    camera.transform.position = new Vector3(0,zoomLevels[2],0);
+        //}
     }
 
 
---
+++ Microscope/Assets/ToggleVis.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class ToggleVis : MonoBehaviour
+{
+    public void toggle()
+    {
+        if (gameObject.activeSelf)
+        {
+            gameObject.SetActive(false);
+        }
+        else
+        {
+            gameObject.SetActive(true);
+        }
+    } 
+}
