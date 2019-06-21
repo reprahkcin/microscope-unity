05911c8d65aa14bf8894153e4ba9d789ac9a50d0
Nick Harper <bignickharper@gmail.com>
2019-06-21 10:59:27 -0700

    working on the lines

--- Microscope/Assets/LabelScript.cs
+++ Microscope/Assets/LabelScript.cs
@@ -2,14 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[ExecuteAlways]
 public class LabelScript : MonoBehaviour
 {
     public GameObject[] objects;
     public GameObject[] labels;
     public Material lineMaterial;
     private GameObject[] lines;
-
+    public GameObject labelGroup;
     private LineRenderer line;
     private Color lineColor = Color.white;
 
@@ -21,10 +20,11 @@ public class LabelScript : MonoBehaviour
             lines[i] = new GameObject();
             lines[i].AddComponent<LineRenderer>();
             lines[i].GetComponent<LineRenderer>().material = lineMaterial;
-            lines[i].GetComponent<LineRenderer>().startColor = lineColor;
-            lines[i].GetComponent<LineRenderer>().endColor = lineColor;
+            lines[i].GetComponent<LineRenderer>().startWidth = 1f;
+            lines[i].GetComponent<LineRenderer>().endWidth = 1f;
             lines[i].GetComponent<LineRenderer>().SetPosition(0,labels[i].transform.position);
             lines[i].GetComponent<LineRenderer>().SetPosition(1, objects[i].transform.position);
+            lines[i].transform.SetParent(labelGroup.transform, false);
         }
 
     }
